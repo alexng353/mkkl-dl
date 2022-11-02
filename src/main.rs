4001a03175613729a9f6f431756197a717a1bdbf
Alexander Ng <alexng353@gmail.com>
2022-11-01 21:12:25 -0700

    fully working; has a ton of comments I need to delete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
-use std::fs;
+use reqwest::header;
+use std::{fs, io::Write};
 use tokio;
 
 // object with ansii color codes
@@ -29,9 +30,20 @@ impl Color {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // get cmd line args
     let c = Color::new();
 
-    let res = reqwest::get("https://mangakakalot.com/read-wg9rm158504883358").await;
+    let args: Vec<String> = std::env::args().collect();
+    // if no length, print no length
+    if args.len() == 1 {
+        // println!(c.red + "No args given" + c.end);
+        println!("{}{}{}", c.red, "No args given", c.end);
+        return Ok(());
+    }
+    let url = &args[1];
+
+    // let res = reqwest::get("https://mangakakalot.com/read-wg9rm158504883358").await; // this is the test url (that works 100%)
+    let res = reqwest::get(url).await;
     let html = res.unwrap().text().await.unwrap();
 
     // regex for <span itemprop="name">something</span>
@@ -67,12 +79,42 @@ async fn main() -> std::io::Result<()> {
     // grab img with the last url
     // let res = reqwest::get(urls[0]).await;
     // println!("{}", urls[urls.clone().len() - 1]);
-    grab_img(&urls[urls.clone().len() - 1]).await;
+    // get_imgs(&urls[urls.clone().len() - 1], "./output/chapter_1").await;
 
+    // get imgs for all chapters but wait 30 seconds between each chapter
+
+    // reverse urls
+    urls.reverse();
+
+    // print all urls
+    // for url in urls.clone() {
+    //     // regex for chapter_{number}?.?{number}
+
+    //     let re = Regex::new(r#"_([0-9]+\.?[0-9]?)"#).unwrap();
+    //     let chapter = re.find(url).unwrap().as_str();
+
+    //     println!("Chapter: {}{}{}", c.green, chapter, c.end);
+    //     // println!("{}", url);
+    // }
+
+    for (i, url) in urls.iter().enumerate() {
+        println!("Downloading chapter {}{}{}", c.green, i + 1, c.end);
+
+        let re = Regex::new(r#"_([0-9]+\.?[0-9]?)"#).unwrap();
+        let chapter = re.find(url).unwrap().as_str();
+
+        get_imgs(url, &format!("./output/chapter{}", chapter)).await;
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    }
+
+    // test().await;
     Ok(())
 }
 
-async fn grab_img(url: &str) {
+async fn get_imgs(url: &str, path: &str) {
+    // make folder path
+    fs::create_dir_all(path).unwrap();
+
     let res = reqwest::get(url).await;
     let html = res.unwrap().text().await.unwrap();
     // regex for <div class="container-chapter-reader">{anything}</div>
@@ -85,6 +127,62 @@ async fn grab_img(url: &str) {
 
     let html = re.captures(&html).unwrap().get(1).unwrap().as_str();
 
-    // let html = re.captures(&res).unwrap();
-    println!("{:?}", html);
+    let re = Regex::new(r#"<img src="([^"]*)"#).unwrap();
+
+    // let re = Regex::new(r#"<img src="(.*)" alt="Devlichi (.*)" title="Devilchi (.*)" />"#).unwrap();
+
+    let matches = re.find_iter(&html);
+
+    let mut urls = Vec::new();
+    for m in matches {
+        urls.push(&m.as_str()[10..m.as_str().len()]);
+    }
+
+    // print all urls
+    // for url in urls.clone() {
+    //     println!("{}", url);
+    // }
+
+    // get an image every 1000 millis
+    println!(
+        "Found {}{:?}{} images",
+        Color::new().green,
+        urls.clone().len(),
+        Color::new().end
+    );
+
+    let mut i = 0;
+    for url in urls.clone() {
+        // let res = reqwest::get(url).await;
+        // let img = res.unwrap().bytes().await.unwrap();
+        // let mut file = fs::File::create(format!("{}.jpg", i)).unwrap();
+        // file.write_all(&img).unwrap();
+        fetch_img(url, &i.to_string(), &path).await;
+        i += 1;
+        tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+    }
+
+    // write html to file
+    // fs::write("test.html", html).expect("Unable to write file");
+}
+
+async fn fetch_img(url: &str, name: &str, path: &str) {
+    let client = reqwest::Client::new();
+
+    let res = client
+        .get(url)
+        .header(header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+        .header(header::REFERER, "https://mangakakalot.com/")
+        .send()
+        .await
+        .unwrap();
+    // let mut new = name.to_string();
+    // while new.len() < 3 {
+    //     new = format!("0{}", name);
+    // }
+    let num = format!("{:0>3}", name);
+    let mut file = fs::File::create(format!("{}/{}.jpg", path, num)).unwrap();
+    file.write_all(&res.bytes().await.unwrap()).unwrap();
+
+    // println!("done");
 }
